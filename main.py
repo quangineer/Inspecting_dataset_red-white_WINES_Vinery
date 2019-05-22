bfb8dc1b7a49ffdbe49895034407b6f7b9dad381
Quan Nguyen <quangineer@gmail.com>
2019-05-21 23:01:23 -0700

    updates

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import pandas as pd
 red_data = pd.read_csv('winequality-red.csv', sep=';')
 white_data = pd.read_csv('winequality-white.csv', sep=';')
 
-print((red_data).info())
+# print((red_data).info())
+# print((red_data).describe())
+# print ((red_data).tail(5))
+# print ((red_data).head(5))
+
+# print((white_data).info())
+# print((white_data).describe())
+# print ((white_data).tail(5))
+# print ((white_data).head(5))
+
+print (red_data.isnull().sum())
+print (white_data.isnull().sum())
