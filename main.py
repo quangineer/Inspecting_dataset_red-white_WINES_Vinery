652df570c68f5853f4465a385a15cb3e9d9619ea
Quan Nguyen <quangineer@gmail.com>
2019-05-21 23:12:01 -0700

    basic inspect

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ red_data = pd.read_csv('winequality-red.csv', sep=';')
 white_data = pd.read_csv('winequality-white.csv', sep=';')
 
 # print((red_data).info())
-# print((red_data).describe())
+print((red_data).describe())
 # print ((red_data).tail(5))
 # print ((red_data).head(5))
 
@@ -13,5 +13,11 @@ white_data = pd.read_csv('winequality-white.csv', sep=';')
 # print ((white_data).tail(5))
 # print ((white_data).head(5))
 
-print (red_data.isnull().sum())
-print (white_data.isnull().sum())
+# print (red_data.isnull().sum())
+# print (white_data.isnull().sum())
+
+# print (white_data.duplicated().sum())
+# print (white_data[white_data.duplicated()])
+
+# print (red_data.nunique())
+# print (white_data.nunique())
