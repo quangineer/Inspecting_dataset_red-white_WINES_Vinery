23219e91c4bdee55d2a103f1a016f287d2b7a797
Quan Nguyen <quangineer@gmail.com>
2019-05-22 18:04:35 -0700

    updates

--- main.py
+++ main.py
@@ -23,5 +23,22 @@ white_data = pd.read_csv('winequality-white.csv', sep=';')
 # print (red_data.nunique())
 # print (white_data.nunique())
 
-print (red_data.shape[0])
-print (white_data.shape[0])
+#give number of row count
+# print (red_data.shape[0])
+# print (white_data.shape[0])
+
+#give number of column count
+# print (red_data.shape[1])
+# print (white_data.shape[1])
+
+#Create color array for red dataframe:
+color_red = np.repeat('red', red_data.shape[0])
+color_white = np.repeat('white', white_data.shape[0])
+
+#Add arrays to the red and white dataframe respectively:
+red_data['color'] = color_red
+white_data['color'] = color_white 
+
+print (red_data.head(10))
+
+print (white_data.head(10))
