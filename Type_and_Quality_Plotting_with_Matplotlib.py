d68c7d58b507788cfabf2883e5d9e1050f75b639
Quan Nguyen <quangineer@gmail.com>
2019-05-25 22:43:37 -0700

    updates

--- Type_and_Quality_Plotting_with_Matplotlib.py
+++ Type_and_Quality_Plotting_with_Matplotlib.py
@@ -18,4 +18,12 @@ wine_df = df.rename(columns = {"Inclusive Sulfur Dioxide" : "inclusive sulfur di
 # 2. WHITE BAR PROPORTIONS = COUNTS FOR EACH QUALITY RATING/TOTAL # OF WHITE SAMPLES
 
 # GET COUNTS/NUMBERS OF EACH RATING AND COLOR:
-color_counts = wine_df.groupby(['color', 'quality']).count()
+color_counts = wine_df.groupby(['color', 'quality']).count()['pH']
+# WHY ['pH']: because of the count, so all the values for all columns are the same, so we just pick a random/arbitrary column.
+color_totals = wine_df.groupby('color').count()['pH']
+
+# get RED proportions by dividing red rating counts by total # of red samples:
+red_proportions = color_counts['red']/color_totals['red']
+
+# get WHITE proportions by dividing white rating counts by total # of white samples:
+white_proportions = color_counts['white']/color_totals['white']
