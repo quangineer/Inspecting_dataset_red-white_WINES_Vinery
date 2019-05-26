0824e29d7030a9eaf35a210cdcf078304a65ce5a
Quan Nguyen <quangineer@gmail.com>
2019-05-25 22:19:01 -0700

    updates

--- Type_and_Quality_Plotting_with_Matplotlib.py
+++ Type_and_Quality_Plotting_with_Matplotlib.py
@@ -1,9 +1,11 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import numpy as np 
+% matplotlib inline
+import seabron as sns
+
 
 ppp = pd.read_csv('winequality_edited.csv')
 
 df = ppp.rename(columns = {"Inclusive Sulfur Dioxide" : "inclusive sulfur dioxide"})
 
-print (list(df.columns))
